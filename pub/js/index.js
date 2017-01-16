cd669fe7efd11edba68563c8e6858fd3ade2ab5e
Adrien <adrien@arccoza.com>
2017-01-16 14:38:55 +0900

    HTML & CSS tidy up.

--- pub/js/index.js
+++ pub/js/index.js
@@ -1,6 +1,5 @@
 $(document)
   .ready(function() {
-    $('.star.rating').rating();
     $('.card .dimmer').dimmer({ on: 'hover' });
     $('.ui.card .ui.more.button').click(function(ev) {
       $($(ev.currentTarget).attr('data-target')).modal('show');
