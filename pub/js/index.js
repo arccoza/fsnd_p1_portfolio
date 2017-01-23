1947ed6965af0e09bee649931ff4bc39112a0753
Adrien <adrien@arccoza.com>
2017-01-23 12:02:59 +0900

    Clean pub dir.

--- pub/js/index.js
+++
@@ -1,7 +0,0 @@
-$(document)
-  .ready(function() {
-    $('.card .dimmer').dimmer({ on: 'hover' });
-    $('.ui.card .ui.more.button').click(function(ev) {
-      $($(ev.currentTarget).attr('data-target')).modal('show');
-    });
-  });
